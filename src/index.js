2f0bfe5ca2d08ea184e9c92a645f27e0343de06e
Ayush <ayushsenapati123@gmail.com>
2021-09-22 23:55:01 +0530

    configured socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, ()=>{
-    console.log(`Server is up on port ${port}!`)
+io.on('connection',()=>{
+    console.log('New WebSocket connection')
 })
 
-//! Goal: Create an Express web server
-//1. Initialize npm and install Express
-//2. Setup a new Express server
-//   - Serve up a public directory
-//   - Listen on port 3000
-//3. Create index.html and render "Chat App" to the screen
-//4. Test your work! Start the server and view the page in the browser
-
-//! Goal: Setup scripts in package.json
-//1. Create a "start" script to start the app using node
-//2. Install nodemon and a developement dependency
-//3. Create a "dev" script to start the app using nodemon
-//4. Run both scripts to test your work!
+server.listen(port, ()=>{
+    console.log(`Server is up on port ${port}!`)
+})
