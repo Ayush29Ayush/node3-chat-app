1869ed832df87f438f95ce5dd24a47c663bf1a17
Ayush <ayushsenapati123@gmail.com>
2021-09-23 12:14:21 +0530

    added disconnect feature

--- src/index.js
+++ src/index.js
@@ -18,28 +18,18 @@ app.use(express.static(publicDirectoryPath))
 //! Server-side Javascript Code
 
 
-//! Goal: Send a Welcome message to new users
-//1. Have server emit "message" when new client connects
-//   - Send "Welcome!" as the event data
-//2. Have client listen for "message" event and print to console
-//3. Test your work!
-
-//! Goal: Allow clients to send messages
-//1. Create a form with an input and button
-//   - Similar to the weather form
-//2. Setup event listener for form submission
-//   - Emit "sendMessage" with input string as message data
-//3. Have server listen for "sendMessage"
-//   - Send message to all connected clients
-//4. Test your work!
-
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection')
 
-    socket.emit('message','Welcome!')
+    socket.emit('message','Welcome!') // emits to everybody
+    socket.broadcast.emit('message', 'A new user has joined') // emits to everybody but not to the current user
 
     socket.on('sendMessage', (message)=>{
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', ()=>{
+        io.emit('message', 'A user has left')
     })
 
 })
