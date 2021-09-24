3147313adbf7943fe1bd4a64da8be4b71407cf4b
Ayush <ayushsenapati123@gmail.com>
2021-09-24 10:37:55 +0530

    form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,21 +2,34 @@
 
 const socket = io()
 
-//! server (emit) -> client (recieve) --acknowledgement--> server
-//! client (emit) -> server (recieve) --acknowledgement--> client
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
 
 socket.on('message', (message)=>{
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
-    e.preventDefault() // to prevent page from refreshing after submission of form
+// $messageForm.document.querySelector('#message-form').addEventListener('submit',(e)=>{
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault() 
+
+    //This area is for => disable the form send button
+    $messageFormButton.setAttribute('disable', 'disabled')
+
 
     // const message = document.querySelector('input').value
     const message = e.target.elements.message.value //! This is alternative of the above line
 
     //! here the 3rd parameter i.e the function runs for acknowledgement
     socket.emit('sendMessage', message, (error)=>{
+        //This area is for => enable the form send button
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = '' // default form entry is empty , also after sending msg it goes blank
+        $messageFormInput.focus() // cursor focuses on form area
+
         // console.log('The message was delivered!')
         if (error) {
             return console.log(error)
@@ -27,10 +40,13 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', ()=>{
+// document.querySelector('#send-location').addEventListener('click', ()=>{
+$sendLocationButton.addEventListener('click', ()=>{
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position)=>{
         // console.log(position)
@@ -39,14 +55,14 @@ document.querySelector('#send-location').addEventListener('click', ()=>{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, ()=>{
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared')
         })
     })
 })
 
-//! Goal: Setup acknowledgement
-
-//1. Setup the client acknowledgement function
-//2. Setup the server to send back the acknowledgement
-//3. have the client print "Location shared!" when acknowledged
-//4. Test your work
+//! Goal: Disable the send location button while location being sent
+//1. Set up a selector at the top of the file
+//2. Disable the button just before getting the current position
+//3. Enable the button in the acknowledgment callback
+//4. Test your work!
