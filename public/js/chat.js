8d1f19c311e749039615210cc5e9cae0619a555e
Ayush <ayushsenapati123@gmail.com>
2021-09-27 21:14:49 +0530

    added scrolling feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,6 +17,31 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+ 
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+ 
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+ 
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+ 
+    // How far have I scrolled?
+    const scrollOffset = Math.ceil($messages.scrollTop) + visibleHeight
+ 
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
@@ -35,6 +60,7 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('roomData', ({ room, users})=>{
