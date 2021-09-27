748248607bfd13abeb2a10b56681a208ce76ebd0
Ayush <ayushsenapati123@gmail.com>
2021-09-27 19:29:18 +0530

    Added and rendered users list

--- src/index.js
+++ src/index.js
@@ -60,6 +60,11 @@ io.on('connection', (socket) => {
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
+        io.to(user.room).emit('roomData',{
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        
         callback()
     })
 
@@ -86,6 +91,10 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 })
